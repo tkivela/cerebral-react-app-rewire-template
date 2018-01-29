145f083e840fcff27d30346ba61b3cc28f90108e
tkivela <kiveltom@gmail.com>
2018-01-29 21:44:07 +0200

    Changed component styling from Emotion's styled components to Emotion's css for better readability.

--- src/controller.js
+++ src/controller.js
@@ -2,13 +2,14 @@ import { Controller } from 'cerebral'
 
 import app from './modules/app'
 
-const Devtools = process.env.NODE_ENV === 'production' ||
-  process.env.NODE_ENV === 'test'
-  ? null
-  : require('cerebral/devtools').default
+const Devtools =
+  process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test'
+    ? null
+    : require('cerebral/devtools').default
 
 export default Controller(app, {
-  devtools: Devtools &&
+  devtools:
+    Devtools &&
     Devtools({
       host: 'localhost:8585',
       // By default the devtools tries to reconnect
